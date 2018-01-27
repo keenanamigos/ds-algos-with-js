87c215ee7f333dbbf664f2b5b8eb528eb09b3373
Keenan Johnson <keenan.johnson1@outlook.com>
2018-01-27 10:29:56 -0500

    Add a private function to get the total number of temperatures for a each week in the month

--- src/chapter-2-arrays/exercises/exercise3.ts
+++ src/chapter-2-arrays/exercises/exercise3.ts
@@ -18,6 +18,19 @@ export class WeeklyTemps {
         this.dataStore.push(temperature);
     }
 
+    // Returns an object with a key for each week and a value representing the number of temperatures
+    private getNumberOfTempsPerWeek(): object {
+        const numberOfTemps = {};
+        const monthOfTemps: number[][] = this.dataStore;
+
+        for (let i = 1; i <= monthOfTemps.length; i++) {
+            // Each i is a new 'week'
+            numberOfTemps[`Week${i}`] = monthOfTemps[i - 1].length;
+        }
+
+        return numberOfTemps;
+    }
+
     // Get the sum of temperatures for each week
     private getSum(...temperatures: number[]): number {
         let sum = 0;
