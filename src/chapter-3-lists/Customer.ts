48d43cb9e00fcada82adf829613616f345a73d03
Keenan Johnson <keenan.johnson1@outlook.com>
2018-02-02 12:18:40 -0600

    Create Customer class

---
+++ src/chapter-3-lists/Customer.ts
@@ -0,0 +1,9 @@
+export class Customer {
+    public name: string;
+    public movie: string;
+
+    public constructor(name: string, movie: string) {
+        this.name = name;
+        this.movie = movie;
+    }
+}
